9ff1cece303acc2afac586b086562e2984f88610
Alex <alex.96_2012@inbox.ru>
2023-10-24 17:02:08 +0900

    fix

--- src/component/Gallery/Gallery.jsx
+++ src/component/Gallery/Gallery.jsx
@@ -3,6 +3,6 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import styles from '../ItemDetail/ItemDetail.module.css'
 export const Gallery = ({ item }) => {
     const images = [];
-    item.map((el) => images.push({ original: 'https://jintai-tech.ru/' + el, thumbnail: 'https://jintai-tech.ru/' + el }));
+    item.map((el) => images.push({ original: 'https://jintai-tech.ru/images' + el, thumbnail: 'https://jintai-tech.ru/images' + el }));
     return <ImageGallery items={images} showPlayButton={false} showBullets={true} showThumbnails={false} additionalClass={styles.image} showFullscreenButton={false} />;
 };
